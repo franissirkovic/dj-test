9aaeef8c6ad325aeaf3d76aaa2e77b80d0681a2e
Franis Sirkovic <franis.sirkovic@gmail.com>
2015-07-03 17:31:09 +0200

    Test simple view.

--- testsite/sampleapp/urls.py
+++ testsite/sampleapp/urls.py
@@ -1,14 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
+import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('sampleapp.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^sampleapp/', include('sampleapp.urls')),
+    url(r'^$', views.HelloWorld, name='HelloWorld')
 )
